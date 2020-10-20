e1f1065e4ce82008abdf984b9058b3f4fdf5b720
Dean Agan <agandfr@gmail.com>
2020-10-20 19:28:58 +1100

    Add implementation

--- app/src/main/kotlin/practice/kotlin/app/threewords/ThreeWords.kt
+++ app/src/main/kotlin/practice/kotlin/app/threewords/ThreeWords.kt
@@ -1,7 +1,13 @@
 package practice.kotlin.app
 
 class ThreeWords {
+
     fun DoesHaveThreeWords(input: String) : Boolean {
-        return true;
+        val splittedInput = input.split(' ')
+        val totalConsecutive = splittedInput.fold(0, {
+            acc: Int, current: String -> acc + (if(current.all { it.isLetter() } || acc >= 3) 1 else -acc )
+        })
+
+        return totalConsecutive >= 3 ;
     }
 }
